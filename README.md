45987707a88bfedcdb28846c02625e2dc56ef661
Krizel <krizzlybear@Nathans-MacBook-Pro.local>
2019-11-04 21:39:45 -0800

    adding bones to html

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # TriviaGame
 UCLA's week 5 coding bootcamp involved creating a trivia game with a timer.
+
+# How to Play
+
+# Languages Used
+* HTML5
+* CSS
+* Javascript
+* jQuery
+
+# Credits
+* Background from [Freepik](https://www.freepik.com/free-vector/colorful-memphis-design-background-vector_3893585.htm)
---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// Global Variables
+
+// Slash page to enter the quiz
+
+// Timer to start the question
+
+// Questions to appear on screen with answers to select
+
+// After an answer is selected, user will be notified whether they're right or wrong.
+
+// Move on to the next question
+
+// Tally all the right and wrong answers
+
+// Announce in the end of the user has won or lost
+
+// Restart game with a new set of questions
