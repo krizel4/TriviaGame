dc731c87ad1be48042d4cbeaf2e2f3ba8ca2d294
Krizel <krizzlybear@Nathans-MacBook-Pro.local>
2019-11-10 19:33:57 -0800

    full game functionality added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,91 +1,217 @@
 $(document).ready(function(){
 
-// Slash page with button to enter the trivia game
-    $("#start-button").on("click", startTrivia.startTimer);
+// GLOBAL VARIABLES
 
+var currentQuestion; // Have one question at a time
+var right; 
+var wrong; 
+var unanswered; 
+var seconds; 
+var time; 
+
+// Variables to trigger new question
+var answered; 
+var userChoice;   
+
+//==============
+// BUTTONS
+//==============
+
+// Hide game in the start    
+    $("#startGame").hide();
+        
+// Start game button
+    $("#startButton").on("click", function(){
+        $(this).hide();
+        newGame();
+    });
+
+// Start a new game
+$("#reset").on("click", function(){
+    $(this).hide();
+    newGame();
 });
-``
-// Global Variables and Trivia Variables
-var questions = []
-var answers 
-var correct = 0;
-var wrong = 0;
-var timer = 10;
-var interval = setInterval(function(){
-  document.getElementById('timer').innerHTML=timer;
-  count--;
-  if (count === 0){
-    clearInterval(interval);
-    document.getElementById('timer').innerHTML='Done';
-  }
-}, 1000);
 
+function newGame(){
+    $("#startGame").show();
+    $("#finalScore").empty();
+    $("#correctAnswers").empty();
+    $("#incorrectAnswers").empty();
+    $("#unanswered").empty();
+    currentQuestion = 0;
+    right = 0;
+    wrong = 0;
+    unanswered = 0;
+    newQuestion();
+}
 
-// User clicks start button to begin trivia
+//==============
+// QUESTIONS
+//==============
 
-// Timer starts when the first question is prompted
+function newQuestion(){
+    
+    // Question number
+    $("#currentQuestion").html("Question " + (currentQuestion+1));
+    $(".question").html(questionBank[currentQuestion].question);
 
-// Questions to appear on screen with answers to select. Questions will rotate randomly through the question bank.
+    // Question loop
+    for(var i = 0; i <= 5; i++){
+        var choices = $("<div>");
+        choices.text(questionBank[currentQuestion].choicesList[i]);
+        choices.attr({"data-index": i });
+        choices.addClass("thisChoice");
+        $(".choicesList").append(choices);
+    }
 
-// After an answer is selected, user will be notified whether they're right or wrong. Add to the correct/wrong scoreboard as it goes along.
+    // Question results with image
+    $("#result").empty();
+    $("#correctedAnswer").empty();
+    answered === true;
 
-// Announce in the end of the user has won or lost
+//==============
+// TIMER
+//==============
+function countdown(){
+    seconds = 10;
+    $("#timeTitle").html("<h3>Time Remaining");    
+    $("#timeLeft").html(seconds);
+    answered === true;
+    time = setInterval(showCountdown, 1000);
+}
 
-// Give the option to play again
+countdown();
 
-// If they chose yes, restart game with a new set of questions
-
-// If they chose no, bring up sad giphy
-
-// Question bank
-
+$(".thisChoice").on("click",function(){
+        userChoice = $(this).data("index");
+        clearInterval(time);
+        answerPage();
+    });
+}
+    
+function showCountdown(){
+    seconds--;
+    if(seconds < 10) {
+        $("#timeLeft").html(seconds);	
+    } else {
+        $("#timeLeft").html(seconds);	
+    }
+    if(seconds < 1){
+        clearInterval(time);
+        answered = false;
+        answerPage();
+    }
+}
+    
+//==============
+// RESULTS & ANSWERS
+//==============
+function answerPage(){
+    $("#currentQuestion").empty();
+    $(".thisChoice").empty(); 
+    $(".question").empty();
+    
+var rightAnswerText = questionBank[currentQuestion].choicesList[questionBank[currentQuestion].answer];
+var rightAnswers = questionBank[currentQuestion].answer;
+    
+// Check answers          
+if((userChoice == rightAnswers) && (answered === true)){
+    right++;
+    $("#result").html(results[result].correct);
+} else if((userChoice != rightAnswers) && (answered === true)){
+    wrong++;
+    $("#result").html(results.incorrect);
+    $("#correctedAnswer").append("The correct answer was: " + "<br/>" + rightAnswerText);
+} else{
+    unanswered++;
+    $("#result").html(results.timeOut);
+    $("#correctedAnswer").append("The correct answer was: " + "<br/>" + rightAnswerText);
+    answered === true;
+}
+if(currentQuestion === (questionBank.length-1)){
+    setTimeout(scoreboard, 2000);
+} else{
+    currentQuestion++;
+    setTimeout(newQuestion, 4000);
+}	
+}
+    
+//==============
+// SCOREBOARD
+//==============
+function scoreboard(){
+    $("#timeLeft").empty();
+    $("#result").empty();
+    $("#correctedAnswer").empty();
+    $("#finalScore").html(results.finished);
+    $("#correctAnswers").html("Right: " + right);
+    $("#wrongAnswers").html("Wrong: " + wrong);
+    $("#unanswered").html("Missed: " + unanswered);
+    $("#reset").addClass("reset");
+    $("#reset").show();
+    $("#reset").html("I'm not superstitious, but I'm a little-stitious. Try again.");
+}
+    
+//==============
+// QUESTION BANK
+//==============
 var questionBank = [
     {question: "When Jim impersonates he gives a list after saying, 'Bears eat beets.' What was the list?",
-    answer: ["Bears. Beets. Battlestar Galactica", "Beets. Bears. Battlestar Galactica", "Beets, bears, beets", "Battlestar Galactica, Beets, Bears" ],
+    choicesList: ["Bears. Beets. Battlestar Galactica", "Beets. Bears. Battlestar Galactica", "Beets, bears, beets", "Battlestar Galactica, Beets, Bears" ],
+    answer: 0,
     correct: "Bears. Beets. Battlestar Galactica."},
 
     {question: "What's not part of Kevin's Famous Chili?",
-    answer: ["Ancho chilis roasted by Kevin himself", "Diced whole tomatoes", "Grilled chicken", "Undercooked onions" ],
+    choicesList: ["Ancho chilis roasted by Kevin himself", "Diced whole tomatoes", "Grilled chicken", "Undercooked onions" ],
+    answer: 2,
     correct: "Grilled chicken"},
 
     {question: "What makes Jim change his mind about interviewing for the VP of Sales?",
-    answer: ["Cold feet", "Athlead", "A foil yogurt top", "Call from Pam" ],
+    choicesList: ["Cold feet", "Athlead", "A foil yogurt top", "Call from Pam" ],
+    answer: 2,
     correct: "A foil yogurt top"},
 
     {question: "What does C.R.I.M.E. A.I.D. stand for?",
-    answer: ["It doesn't stand for anything", "Crime Reduces Innoncence Makes Every Angry I Declare","Crime Really Isn't Making Everyone Angry I Declare", "CRIME Anger Innoncence Declaration" ],
+    choicesList: ["It doesn't stand for anything", "Crime Reduces Innoncence Makes Every Angry I Declare","Crime Really Isn't Making Everyone Angry I Declare", "CRIME Anger Innoncence Declaration" ],
+    answer: 1,
     correct: "Crime Reduces Innocence Makes Everyone Angry I Declare"},
 
     {question: "What's the name of Michael Scarn's robot butler?",
-    answer: ["Samuel", "Joseph", "Dwayne", "Derrik" ],
-    correct: "Samuel"},
-
-    {question: "What's the name of Michael Scarn's robot butler?",
-    answer: ["Samuel", "Joseph", "Dwayne", "Derrik" ],
+    choicesList: ["Samuel", "Joseph", "Dwayne", "Derrik" ],
+    answer: 0, 
     correct: "Samuel"},
 
     {question: "In the pilot, who started their first day at Dunder Mifflin Scranton?",
-    answer: ["Jim Halpert", "Creed Bratton", "Meredith Palmer", "Ryan Howard"],
+    choicesList: ["Jim Halpert", "Creed Bratton", "Meredith Palmer", "Ryan Howard"],
+    answer: 3,
     correct: "Ryan Howard"},
-  
-    {question: "In season 2, what Dundie award does Phyllis take home?",
-    answer: ["Bushiest Beaver", "Spicy Curry", "Whitest Sneakers", "Busiest Beaver"],
-    correct: "Bushiest Beaver"},
 
     {question: "Which of these things does Prison Mike NOT claim to have been busted for?",
-    answer: ["Hurt a dementor", "Robbed", "Stole", "Kidnapped president's son"],
+    choicesList: ["Stole", "Robbed", "Hurt a dementor", "Kidnapped president's son"],
+    answer: 2,
     correct: "Hurt a dementor"},
 
     {question: "How many vasectomies does Michael claim to have had?",
-    answer: ["One", "Two", "Three", "Four"],
+    choicesList: ["One", "Two", "Three", "Four"],
+    answer: 2,
     correct: "Three"},
 
     {question: "What musical did Jan and Michael see while dating? (Hint: It was mentioned at the dinner party in season 4.)",
-    answer: ["Cats", "Wicked", "Stomp", "Rent"],
+    choicesList: ["Cats", "Wicked", "Stomp", "Rent"],
+    answer: 1,
     correct: "Wicked"},
 
     {question: "When Pam was office manager in Season 7, she made everyone write down their New Year's Resolution. What was Michael's?",
-    answer: ["Floss", "To do a cartwheel", "Help Kevin eat broccoli", "Drink less caffeine"],
+    choicesList: ["Floss", "To do a cartwheel", "Help Kevin eat broccoli", "Drink less caffeine"],
+    answer: 0,
     correct: "Floss"},
+    ]
 
-]
+//==============
+// RESULTS
+//==============
+var results = {correct: "You and I are soup snakes.",
+    wrong: "Before I do anything I ask myself, 'Would an idiot do that?' and if the answer is yes, I do not do that thing.",
+    timeOut: "Times up. You all took a life here today. The life of the party."
+    };
+}); 
